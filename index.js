b695b3768b368057231e9bb447a6f7036b5d87f6
Jonathan Ong <jonathanrichardong@gmail.com>
2013-11-30 10:21:32 -0800

    fix empty files

--- index.js
+++ index.js
@@ -46,6 +46,9 @@ module.exports = function (request, options) {
   }
 
   function onFile(fieldname, file, filename, encoding, mimetype) {
+    https://github.com/mscdex/busboy/issues/16
+    if (!filename)
+      return file.resume()
     // opinionated, but 5 arguments is ridiculous
     file.fieldname = fieldname
     file.filename = filename
